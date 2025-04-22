c7a7b3ce3cc88df07898c3a86072f5eb2504ce4b
NBkun <nguyennbkun@gmail.com>
2025-04-22 21:53:43 +0700

    fixed 1 typo, added path input function from CLI and 2 more books(mobydick and prideandjustice but they are ignored by .gitignore tho)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import *
+import sys
 
 def get_book_text(path):
     with open(path) as f:
@@ -6,13 +7,16 @@ def get_book_text(path):
         return file_content
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
     char_count = get_num_chars(get_book_text(path))
     num_words = get_num_words(get_book_text(path))
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path}")
     print("----------- Word Count ----------")
-    print(f"found {num_words} total words")
+    print(f"Found {num_words} total words")
     print("--------- Character Count -------")
     for char, count in char_count:
         if char.isalpha():
